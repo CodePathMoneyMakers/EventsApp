75682cf7468e5fc97a0111b38dcda0a04ef0834e
Daniela Agueros <viodani01@gmail.com>
2021-03-24 18:11:22 -0400

    updated firebase - changed dependencies

--- app/src/main/java/com/example/eventsapp/LoginActivity.java
+++ app/src/main/java/com/example/eventsapp/LoginActivity.java
@@ -1,14 +1,23 @@
 package com.example.eventsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -20,6 +29,9 @@ public class LoginActivity extends AppCompatActivity {
     TextView tvCreateAccount, tvForgotPassword;
     Button btnLogin;
 
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +42,7 @@ public class LoginActivity extends AppCompatActivity {
         tvCreateAccount = findViewById(R.id.tvCreateAccount);
         tvForgotPassword = findViewById(R.id.tvForgotPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        mAuth = FirebaseAuth.getInstance();
 
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
@@ -37,8 +50,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Log.e(TAG, "inside onClick");
 
-                Intent i = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(i);
+               userLogin(); // calls login method
             }
         });
 
@@ -53,10 +65,47 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    private void userLogin() {
+        String email = etEmail.getText().toString().trim();
+        String password =etPassword.getText().toString().trim();
 
+        if(email.isEmpty()){
+            etEmail.setError("Email is required");
+            etEmail.requestFocus();
+            return;
+        }
 
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            etEmail.setError("Please enter a valid email");
+            etEmail.requestFocus();
+            return;
+        }
 
+        if(password.isEmpty()){
+            etPassword.setError("Password is required");
+            etPassword.requestFocus();
+            return;
+        }
 
+        if(password.length() < 6){
+            etPassword.setError("Minimum password length is 6 characters");
+            etPassword.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    // redirect to user profile
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, "Failed to login! Please check your credentials!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
 
 
 }
