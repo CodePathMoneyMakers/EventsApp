cfa9969cccb6b31572fd30eeadb5f257ff22df99
Daniela Agueros <viodani01@gmail.com>
2021-05-28 13:06:52 -0400

    merged

--- app/src/main/java/com/example/eventsapp/DetailsActivity.java
+++ app/src/main/java/com/example/eventsapp/DetailsActivity.java
@@ -1,6 +1,7 @@
 package com.example.eventsapp;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
